a37cd250dd1f275c0593ccdffbbf47036abd03d2
Duhan Muhammad Aziz <emailkamu@gmail.com>
2026-07-06 14:20:19 +0700

    fix: set associated member pelatih_id to NULL before deleting coach record

--- admin/hapus_pelatih.php
+++ admin/hapus_pelatih.php
@@ -7,6 +7,7 @@ include '../includes/koneksi.php';
 
 $id = mysqli_real_escape_string($koneksi, $_GET['id']);
 
+$q_member = mysqli_query($koneksi, "UPDATE member SET pelatih_id = NULL WHERE pelatih_id='$id'");
 $q = mysqli_query($koneksi, "DELETE FROM pelatih WHERE id='$id'");
 
 if($q) {
